3a979951fa38a8b7114870d4277fdf28da076455
Lame-desu <lamesgindesalegn30@gmail.com>
2024-12-12 03:32:04 +0300

    modified router by importing protected route correctly

--- src/Router.jsx
+++ src/Router.jsx
@@ -10,8 +10,9 @@ import ProductDetail from "./pages/ProductDetail/ProductDetail";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
-import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
+// import ProtectedRoute from './Components/ProtectedRoute/ProtectedRoute'
 
+//pk_test_51Q0sZLA7KsFf5YYB14hGAubGWk5mxCO4QCuegYhFLDMaCuA7Q1dnj9tZ7VXDdcZ2aaZQi4sRZtXyalNsOEzeEHeX00tujhBuZX
 const stripePromise = loadStripe(
   "pk_test_51QUdvsJAQXvyp5PMmK91CpDyuW2sSSvxP0RTBVH4q1zsEcegPnhBBR7487GFtVf7ikZj5cv3lrglY079D94GXQhC00IjZyMNtX"
 );
@@ -19,34 +20,22 @@ function Routing() {
   return (
     <Router>
       <Routes>
+        {/* <Route path = "/" element = {<LayOut />}> */}
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Auth />} />
         <Route
           path="/payments"
           element={
-            <ProtectedRoute msg={"you must login to pay"} redirect="/payments">
-              <Elements stripe={stripePromise}>
-                <Payment />
-              </Elements>
-            </ProtectedRoute>
+            <Elements stripe={stripePromise}>
+              <Payment />
+            </Elements>
           }
         />
-        <Route
-          path="/orders"
-          element={
-            <ProtectedRoute
-              msg={"you must login to access your orders"}
-              redirect="/orders"
-            >
-              <Elements stripe={stripePromise}>
-                <Orders />
-              </Elements>
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/orders" element={<Orders />} />
         <Route path="/category/:categoryName" element={<Results />} />
         <Route path="/products/:productId" element={<ProductDetail />} />
         <Route path="/cart" element={<Cart />} />
+        {/* </Route> */}
       </Routes>
     </Router>
   );
